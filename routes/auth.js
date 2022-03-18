57b8a7babb5a84aa9e7315c401fd2f801b8e5100
Venkata Sridhar Sai Kalavalapalli <kv.sridharsai@gmail.com>
2022-03-19 00:07:20 +0530

    passport strategy set

--- routes/auth.js
+++
@@ -1,5 +0,0 @@
-var express = require("express");
-var passport = require("passport");
-var LocalStrategy = require("passport-local");
-const mongoose = require("mongoose");
-const User = require("../models/users");
