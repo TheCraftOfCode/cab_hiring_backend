31e3533b0d34485dd29a32496412e7349aba7b7e
Venkata Sridhar Sai Kalavalapalli <kv.sridharsai@gmail.com>
2022-03-21 14:34:43 +0530

    login and sign up done

--- models/users.js
+++ models/users.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const users = mongoose.Schema({
     name: {
@@ -17,9 +20,23 @@ const users = mongoose.Schema({
     phone: {
         type: String,
         required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+        default: false
     }
 });
 
+users.methods.GenerateJwtToken = function () {
+    const token = jwt.sign(
+      { _id: this._id, username: this.username, isAdmin: this.isAdmin },
+      process.env.JWT_SECRET_KEY
+    );
+    return token;
+  };
+
 const Users = mongoose.model("users", users);
+
 
 module.exports = Users;
--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,27 @@
-const mongoose = require("mongoose");
-const express = require("express");
-const router = express.Router();
-const passport = require("passport");
-const passportlocal = require("passport-local");
+const Express = require("express");
+const bcrypt = require("bcrypt");
+const router = Express.Router();
+const User = require("../models/users");
 
 router.post("/", async (request, response) => {
   const { username, password } = request.body;
+  //checking the database for the user.
+  const CheckUser = await User.findOne({ username: username });
+
+  //if user record is not available then responds as a bad request
+  if (!CheckUser)
+    return response.status(400).send("Invalid Username or Password");
+
+  //check if entered password and user password is same
+  const PasswordCheck = await bcrypt.compare(password, CheckUser.password);
+  //wrong password case
+  if (!PasswordCheck)
+    return response.status(400).send("Invalid Username or Password");
+
+  //if both entered username and password are correct then generating a jwt token
+
+  const token = CheckUser.GenerateJwtToken();
+  response.header("user-auth-token", token).status(200).send(token);
 });
 
 module.exports = router;
