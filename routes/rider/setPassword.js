34e7eac5e6d1b551a518aa62c7d6242e6cd44216
Venkata Sridhar Sai Kalavalapalli <kv.sridharsai@gmail.com>
2022-03-23 22:45:49 +0530

    verify Otp and setPassword is up

---
+++ routes/rider/setPassword.js
@@ -0,0 +1,32 @@
+const express = require("express ");
+const router = express.Router();
+const mongoose = require("mongoose");
+const user = require("../../models/users");
+
+router.put("/", async (request, response) => {
+  const { email, Newpassword } = request.body;
+
+  const retrieve = await user.findOne({ email: email });
+
+  if (!retrieve) {
+    response.status(400).send("Operation is not valid");
+  } else {
+    user.findOneAndUpdate(
+      { email: email },
+      { password: Newpassword },
+      null,
+      function (err, docs) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Original Doc : ", docs);
+          response
+            .status(200)
+            .send("Password change successfull..Please Log in");
+        }
+      }
+    );
+  }
+});
+
+module.exports = router;
---
+++ routes/rider/verifyOtp.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const otpdump = require(".../models/otpdump");
+const router = express.Router();
+
+router.post("/", async (request, response) => {
+  const { email, otp } = request.body;
+
+  const retrieve = await otpdump.findOne({ email: email, otp: otp });
+
+  if (!retrieve) {
+    response.status(400).send("Invalid Otp (or) Otp Expired");
+  }
+  //check if the otp is valid if email is found
+  var OtpStatus = checkifOtpIsValid(otp);
+
+  if (OtpStatus) {
+    //redirect to set new password page
+    response.redirect();
+    reponse.status(200).send("Otp is valid..! Render reset password page");
+  } else {
+    response.status(400).send("Otp is invalid");
+  }
+});
+
+async function checkifOtpIsValid(email, otp) {
+  const ActualOtp = await otpdump.findOne({ email: email });
+  if (ActualOtp.otp === otp) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+module.exports = router;
