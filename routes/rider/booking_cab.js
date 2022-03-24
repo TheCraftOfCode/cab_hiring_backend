18226dc5b9bc2b3d5cc215ed1ebecd0f30b729aa
Venkata Sridhar Sai Kalavalapalli <kv.sridharsai@gmail.com>
2022-03-24 11:37:12 +0530

    booking route module done

---
+++ routes/rider/booking_cab.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const bookingModel = require(".../models/user_cab_booking");
+
+router.post("/", async (request, response) => {
+  const {
+    pickup_location,
+    drop_location,
+    pickup_time,
+    promocode,
+    cab_type,
+    cab_number,
+  } = request.body;
+
+  const bookingData = new bookingModel({
+    pickup_location,
+    drop_location,
+    pickup_time,
+    promocode,
+    cab_type,
+    cab_number,
+  });
+
+  const bookingStatus = await bookingData.save();
+
+  bookingStatus.then(() => {
+    response.status(201).send("Booking Successful");
+  });
+  bookingStatus.catch((err) => {
+    response
+      .status(400)
+      .send(`Something went wrong.. Please Try again \n ${err}`);
+  });
+});
+
+module.exports = router;
