5522c7580d19261d6cc1a328970d489525811092
Venkata Sridhar Sai Kalavalapalli <kv.sridharsai@gmail.com>
2022-05-16 17:27:39 +0530

    closes #4 for heroku backup

--- routes/rider/complaints_customer_service.js
+++ routes/rider/complaints_customer_service.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-// const complaintModel = require(".../models/complaint");
 const complaintModel = require("../../models/complaint");
 
 router.post("/", async (request, response) => {
@@ -12,17 +11,15 @@ router.post("/", async (request, response) => {
   });
   const complaintAck = await complaintData.save();
 
-  complaintAck
-    .then(() => {
-      response
-        .status(201)
-        .send(
-          "Thanks for the complaint..! Your issue has been raised.Our team will contact you soon"
-        );
-    })
-    .catch((err) => {
-      response.status(400).send(`Please try again \n ${err}`);
-    });
+  if (complaintAck) {
+    response
+      .status(201)
+      .send(
+        "Thanks for the complaint..! Your issue has been raised.Our team will contact you soon"
+      );
+  } else {
+    response.status(400).send(`Please try again \n ${err}`);
+  }
 });
 
 module.exports = router;
