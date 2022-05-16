58b24ba8b0f12101fe06c4456ee18cefc15687d6
Venkata Sridhar Sai Kalavalapalli <kv.sridharsai@gmail.com>
2022-05-16 17:01:34 +0530

    closes #9 booking cab endpoint up and tested using postman

--- Helper functions/generateRandom.js
+++ Helper functions/generateRandom.js
@@ -2,4 +2,15 @@ function generateAnSixDigitRandomOtp() {
   return Math.floor(100000 + Math.random() * 900000);
 }
 
-module.exports = generateAnSixDigitRandomOtp;
+function generateARandomTwoDigit() {
+  return Math.floor(Math.random() * 90 + 10);
+}
+function getRandomArbitrary(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+module.exports = {
+  generateAnSixDigitRandomOtp,
+  generateARandomTwoDigit,
+  getRandomArbitrary,
+};
--- routes/rider/booking_cab.js
+++ routes/rider/booking_cab.js
@@ -2,15 +2,33 @@ const express = require("express");
 const router = express.Router();
 const bookingModel = require("../../models/user_cab_booking");
 
+const {
+  generateARandomTwoDigit,
+  getRandomArbitrary,
+} = require("../../Helper functions/generateRandom");
+
+function suffix() {
+  const alphabets = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  return alphabets[getRandomArbitrary(1, 36)];
+}
+
+function GenerateACabNumber() {
+  var finalNumberPlate;
+  const prefix = "TN";
+  const twoDigit = generateARandomTwoDigit();
+  finalNumberPlate = prefix + twoDigit.toString();
+  for (var i = 0; i < 4; i++) {
+    finalNumberPlate = finalNumberPlate + suffix();
+  }
+  return finalNumberPlate;
+}
+
 router.post("/", async (request, response) => {
-  const {
-    pickup_location,
-    drop_location,
-    pickup_time,
-    promocode,
-    cab_type,
-    cab_number,
-  } = request.body;
+  const { pickup_location, drop_location, pickup_time, promocode, cab_type } =
+    request.body;
+
+  //allocate a cab No random number
+  var cab_number = GenerateACabNumber();
 
   const bookingData = new bookingModel({
     pickup_location,
@@ -23,15 +41,13 @@ router.post("/", async (request, response) => {
 
   const bookingStatus = await bookingData.save();
 
-  bookingStatus.then(() => {
-    response.status(201).send("Booking Successful");
-  });
-
-  bookingStatus.catch((err) => {
+  if (bookingStatus) {
+    response.status(201).send(bookingStatus);
+  } else {
     response
       .status(400)
       .send(`Something went wrong.. Please Try again \n ${err}`);
-  });
+  }
 });
 
 module.exports = router;
